394a98e17fa26b10074db890673935972a4d9ad2
Ajax Tchakounte <atchakou@iphone-2.myfiosgateway.com>
2026-06-10 09:31:25 -0400

    Added React useState to App to track and setTaks from array

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 import "./App.css";
 import ToDoListTask from "./components/ToDoListTask/ToDoListTask";
+import React from "react";
 
 function App() {
+  const [tasks, setTasks] = React.useState([
+    {
+      taskNum: "1",
+      task: "Book round-flight ticket and reserve 3 nights at hotel.",
+    },
+    {
+      taskNum: "2",
+      task: "Gather W-2s and enter charitable donation receipts into the spreadsheet.",
+    },
+    {
+      taskNum: "3",
+      task: "Wipe down kitchen counters and vacuum the living room rug.",
+    },
+    {
+      taskNum: "4",
+      task: "Call pharmacy to request a refill on blood pressure medication.",
+    },
+    {
+      taskNum: "5",
+      task: "Call Mom and Dad to catch up; check time zones first.",
+    },
+  ]);
+
   return (
     <div className="App">
       <header className="App-header">To-Do List App</header>
       <main className="to-do-list-container">
-        <ToDoListTask
-          taskNum="1"
-          task="Book round-flight ticket and reserve 3 nights at hotel."
-        />
-        <ToDoListTask
-          taskNum="2"
-          task="Gather W-2s and enter charitable donation receipts into the spreadsheet."
-        />
-        <ToDoListTask
-          taskNum="3"
-          task="Wipe down kitchen counters and vacuum the living room rug."
-        />
-        <ToDoListTask
-          taskNum="4"
-          task="Call pharmacy to request a refill on blood pressure medication."
-        />
-        <ToDoListTask
-          taskNum="5"
-          task=" Call Mom and Dad to catch up; check time zones first."
-        />
+        <ToDoListTask taskNum={tasks[0].taskNum} task={tasks[0].task} />
+        <ToDoListTask taskNum={tasks[1].taskNum} task={tasks[1].task} />
+        <ToDoListTask taskNum={tasks[2].taskNum} task={tasks[2].task} />
+        <ToDoListTask taskNum={tasks[3].taskNum} task={tasks[3].task} />
+        <ToDoListTask taskNum={tasks[4].taskNum} task={tasks[4].task} />
       </main>
     </div>
   );
