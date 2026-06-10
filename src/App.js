11f9520db58413a6499ef65b0252f61d82bd1331
Ajax Tchakounte <atchakou@iphone-2.myfiosgateway.com>
2026-06-09 21:31:56 -0400

    Added new ListItem components to App

--- src/App.js
+++ src/App.js
@@ -6,8 +6,22 @@ function App() {
     <div className="App">
       <header className="App-header">To-Do List App</header>
       <main>
-        <ListItem taskNum="1" task="work on project" />
-        <ListItem taskNum="2" task="file your taxes" />
+        <ListItem
+          taskNum="1"
+          task="Book round-flight ticket and reserve 3 nights at hotel"
+        />
+        <ListItem
+          taskNum="2"
+          task="Gather W-2s and enter charitable donation receipts into the spreadsheet."
+        />
+        <ListItem
+          taskNum="3"
+          task="Wipe down kitchen counters and vacuum the living room rug."
+        />
+        <ListItem
+          taskNum="4"
+          task="Call pharmacy to request a refill on blood pressure medication."
+        />
       </main>
     </div>
   );
