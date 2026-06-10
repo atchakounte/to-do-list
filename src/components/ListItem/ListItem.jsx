44be0c504b546e8f4bccbd032b35d5b16c71ab99
Ajax Tchakounte <atchakou@iphone-2.myfiosgateway.com>
2026-06-09 20:58:10 -0400

    Destructured props in functional component ListItem

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./ListItem.css";
 
-export default function ListItem(props) {
+export default function ListItem({ taskNum, task }) {
   return (
     <div className="list-item">
       <p>
-        task {props.taskNum}: {props.task}
+        task {taskNum}: {task}
       </p>
     </div>
   );
