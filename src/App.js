f6b288a23adcfe3cbb2b1dc790bd55d31fc05519
Ajax Tchakounte <atchakou@iphone-2.myfiosgateway.com>
2026-06-09 21:00:59 -0400

    Added new ListItem component to App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import "./App.css";
+import ListItem from "./components/ListItem/ListItem";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">To-Do List App</header>
+      <main>
+        <ListItem taskNum="1" task="work on project" />
+        <ListItem taskNum="2" task="file your taxes" />
+      </main>
     </div>
   );
 }
