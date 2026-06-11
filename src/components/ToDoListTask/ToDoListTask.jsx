5e675141934f5def369b702526a115b3b6cbfa40
Ajax Tchakounte <atchakou@samsung.myfiosgateway.com>
2026-06-10 20:45:06 -0400

    Added event click handle for ToDoListTask

--- src/components/ToDoListTask/ToDoListTask.jsx
+++ src/components/ToDoListTask/ToDoListTask.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import { useState } from "react";
 import "./ToDoListTask.css";
 
 export default function ToDoListTask({ taskNum, task }) {
-  const [isTaskCompleted, setTaskCompleted] = React.useState(false);
+  const [isTaskCompleted, setTaskCompleted] = useState(false);
 
   const taskContent = !isTaskCompleted ? (
     <p>
@@ -14,7 +14,16 @@ export default function ToDoListTask({ taskNum, task }) {
     </p>
   );
 
-  return <div className="task-item">{taskContent}</div>;
+  const handleClick = (/*e*/) => {
+    // e.preventDefault(); // in case of form fields
+    setTaskCompleted(!isTaskCompleted);
+  };
+
+  return (
+    <div className="task-item" onClick={handleClick}>
+      {taskContent}
+    </div>
+  );
 }
 
 // export default ToDoListTask;
