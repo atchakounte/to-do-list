9edd53bd33ce7325190dc2d621ec36065bdb9e24
Ajax Tchakounte <atchakou@samsung.myfiosgateway.com>
2026-06-13 16:07:03 -0400

    Added taskList mapping to TasksContainer

--- src/components/TasksContainer/TasksContainer.jsx
+++ src/components/TasksContainer/TasksContainer.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import ToDoListTask from "../ToDoListTask/ToDoListTask";
 import "./TasksContainer.css";
 
-export default function TasksContainer({ taskList }) {
+export default function TasksContainer({ tasks }) {
+  const taskList = tasks.map((myTask) => (
+    <ToDoListTask
+      taskNum={myTask.taskNum}
+      task={myTask.task}
+      key={myTask.task}
+    />
+  ));
   return <section className="tasks-container">{taskList}</section>;
 }
